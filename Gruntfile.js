574d08ba56ee6dacc6537a17f3de0acab3057be7
dliendo <dliendo@somoswoxi.com>
2024-04-08 12:37:51 -0300

    changed pkg name

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['src/dom-to-image.js']
+                    'dist/<%= pkg.name %>.min.js': ['src/dom-to-image-fontless.js']
                 }
             }
         },
@@ -32,10 +32,10 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-karma');
+    // grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('test', ['karma']);
-    grunt.registerTask('default', ['jshint', 'test', 'uglify']);
+    // grunt.registerTask('test', ['karma']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
